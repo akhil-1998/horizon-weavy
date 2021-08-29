3afafe7d03ad499dce022fd6190d010404abfae8
Arjun Raj <arjunraj.pala@gmail.com>
2021-08-30 00:31:47 +0530

    Initial commit

---
+++ src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Arjun Raj',
+  email: 'arjunraj.pala@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg'
+};
+
+export default account;
